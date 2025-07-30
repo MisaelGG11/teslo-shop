977219c864993447ed8ad9c4bb287732fc0e2816
MisaelGG11 <magomez@innovacion.gob.sv>
2025-07-30 10:54:00 -0600

    feat: add title component

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -6,7 +6,7 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex flex-col min-h-screen">
+    <main className="flex flex-col min-h-screen px-0 sm:px-10">
       <TopMenu />
       {children}
     </main>
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,8 +1,13 @@
+import { Title } from "@/components/ui/Title/Title";
 import { titleFont } from "@/config/fonts";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className={`${ titleFont.className} font-bold`}>Hola</main>
+    <>
+      <main className={`${titleFont.className} font-bold`}>
+        <Title title="Welcome to the Shop" subTitle="Find the best products here." className="mb-2"/>
+      </main>
+    </>
   );
 }
