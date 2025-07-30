ba0ef3599dde5604c0ff5129b3610a382b93644a
MisaelGG11 <magomez@innovacion.gob.sv>
2025-07-30 15:43:45 -0600

    feat: initial implementation of product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,49 @@
-export default async function ProductPage() {
+import { titleFont } from "@/config/fonts";
+import { Product } from "@/interfaces";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+const products: Product[] = initialData.products;
+
+interface Props {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function ProductPage({ params }: Props) {
+  const { slug } = await params;
+
+  const product = products.find((prod) => prod.slug === slug);
+
+  if (!product) {
+    return notFound();
+  }
+
   return (
-    <main className="p-4">
-      <h1 className="text-2xl font-bold">Product Details</h1>
-      <p>Displaying details for product</p>
+    <main className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2 ">
+      </div>
+
+      {/* Detalles */}
+      <div className="col-span-1 px-5">
+        <h1 className={` ${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price.toFixed(2)}</p>
+
+        {/* Selector de Tallas */}
+
+
+        {/* Selector de Cantidad */}
+
+
+        {/* Button */}
+        <button className="btn-primary my-5">Agregar al carrito</button>
+
+        {/* Descripción */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </main>
   );
 }
