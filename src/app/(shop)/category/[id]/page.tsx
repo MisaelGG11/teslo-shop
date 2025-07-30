1dcbc73d18f2f1fc204af108aa793bd6789c814c
MisaelGG11 <magomez@innovacion.gob.sv>
2025-07-30 10:38:17 -0600

    feat: add not found page for categories section

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,9 +1,22 @@
-export default async function CategoryPage() {
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+export default async function CategoryPage({ params }: Props) {
+  const { id } = await params;
+
+  const validCategories = [ 'men', 'women', 'children' ];
+
+  if (!validCategories.includes(id)) {
+    return notFound();
+  }
 
   return (
     <main className="p-4">
-      <h1 className="text-2xl font-bold">Category: 1</h1>
-      <p>Displaying products for category</p>
+      <h1 className="text-2xl font-bold">Category: {id}</h1>
+      <p>Displaying products for category {id}</p>
     </main>
   );
 }
---
+++ src/app/(shop)/category/not-found.tsx
@@ -0,0 +1,35 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="flex flex-reverse md:flex-row h-[700px] w-full justify-center items-center align-middle">
+      <div className="text-center px-5 mx-5">
+        <h1 className={`${titleFont.className} antialiased text-9xl`}>404</h1>
+        <p className="font-semibold text-xl">
+          Lo sentimos, la categoría que buscas no existe.
+        </p>
+        <p className="font-light">
+          <span className="">
+            Verifica la URL o regresa a la página principal - {" "}
+          </span>
+          <Link href="/" className="transition-all duration-500 ease-in-out hover:underline">
+            Inicio
+          </Link>
+        </p>
+      </div>
+
+
+      <div className="px-5 mx-5">
+        <Image
+          src="/imgs/starman_750x750.png"
+          alt="Starman"
+          width={400}
+          height={400}
+          className="rounded-lg p-5 sm:p-0"
+        />
+      </div>
+    </main>
+  );
+}
