dfd6a33f5847aa8f807a99c1b881857ca94905c6
MisaelGG11 <magomez@innovacion.gob.sv>
2025-07-31 10:33:05 -0600

    feat: add product slideshow component for desktop devices

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { titleFont } from "@/config/fonts";
 import { Product } from "@/interfaces";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
-import { SizeSelector, QuantitySelector } from "@/components";
+import { SizeSelector, QuantitySelector, Slideshow } from "@/components";
 
 const products: Product[] = initialData.products;
 
@@ -23,6 +23,11 @@ export default async function ProductPage({ params }: Props) {
     <main className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Slideshow */}
       <div className="col-span-1 md:col-span-2 ">
+        <Slideshow 
+          images={product.images}
+          title={product.title}
+          className="h-full"
+        />
       </div>
 
       {/* Detalles */}
