14943528523b76162f13f0b87511db89715e56e1
MisaelGG11 <magomez@innovacion.gob.sv>
2025-08-07 14:26:06 -0600

    feat: add auth layout with login a register pages

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,11 +4,10 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col">
-      <header className="bg-teal-800 text-white p-4">
-        <h1 className="text-2xl font-bold">Auth</h1>
-      </header>
-      <main className="flex-1 p-4">{children}</main>
+    <div className="flex justify-center items-center">
+      <div className="w-full max-w-md px-10">
+        {children}
+      </div>
     </div>
   );
 }
--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,3 +1,6 @@
+import { titleFont } from "@/config/fonts";
+import Link from "next/link";
+
 export const metadata = {
   title: "New Account",
   description: "Create a new account",
@@ -5,10 +8,49 @@ export const metadata = {
 
 export default async function NewAccountPage() {
   return (
-    <main className="flex items-center justify-center h-screen">
-      <div className="w-full max-w-md p-8 bg-white rounded shadow-md">
-        <h2 className="text-2xl font-bold mb-6">Create New Account</h2>
-        {/* New account form goes here */}
+ <main className="flex flex-col min-h-screen justify-center">
+
+      <h1 className={ `${ titleFont.className } text-4xl mb-5` }>Nueva cuenta</h1>
+
+      <div className="flex flex-col">
+
+        <label htmlFor="email">Nombre completo</label>
+        <input
+          className="px-5 py-2 border border-gray-400 bg-gray-200 rounded mb-5"
+          type="text" />
+
+
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          className="px-5 py-2 border border-gray-400 bg-gray-200 rounded mb-5"
+          type="email" />
+
+
+        <label htmlFor="email">Contraseña</label>
+        <input
+          className="px-5 py-2 border border-gray-400 bg-gray-200 rounded mb-5"
+          type="email" />
+
+        <button
+          
+          className="btn-primary">
+          Crear cuenta
+        </button>
+
+
+        {/* divisor l ine */ }
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">O</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <Link
+          href="/auth/login" 
+          className="btn-secondary text-center">
+          Ingresar
+        </Link>
+
       </div>
     </main>
   );
