152ceafb6c5f246a8a9e073970e2318b9c5a6ec3
MisaelGG11 <magomez@innovacion.gob.sv>
2025-08-08 15:47:14 -0600

    feat: handle products pagination from server side

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,31 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import { Title, ProductGrid } from "@/components";
 import { titleFont } from "@/config/fonts";
-import { Product } from "@/interfaces";
-import { initialData } from "@/seed/seed";
+import { Gender } from "@/generated/prisma";
+import { redirect } from "next/navigation";
 
-const products: Product[] = initialData.products;
+interface Props {
+  searchParams: Promise<{ page?: string, take?: string, gender?: string }>;
+}
 
-export default function Home() {
+export default async function Home({ searchParams }: Props) {
+  const params = await searchParams;
+  const page = params.page ? parseInt(params.page) : 1;
+  const take = params.take ? parseInt(params.take) : 12;
+
+  const { products, totalPages, currentPage } = await getPaginatedProductsWithImages({
+    page,
+    take,
+    gender: params.gender as Gender,
+  });
+
+  console.log("Current Page:", currentPage);
+  console.log("Total Pages:", totalPages);
+
+  if(products.length === 0) {
+    redirect('/');
+  }
+
   return (
     <>
       <main className={`${titleFont.className} font-bold px-5 md:px-0`}>
